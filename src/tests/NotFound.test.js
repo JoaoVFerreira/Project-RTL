7cfe49e7279b00dd3690483538a72b68230ccb4c
João Ferreira <joaovtf39@gmail.com>
2021-09-29 18:02:20 -0300

    requisito 4 OK

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helper/renderWithRouter';
+import { NotFound } from '../components';
+
+test('Requisito 4', () => {
+  renderWithRouter(<NotFound />);
+
+  const textNotFound = screen.getByRole('heading',
+    { name: 'Page requested not found Crying emoji', level: 2 });
+
+  expect(textNotFound).toBeInTheDocument();
+
+  const noFoundIMG = screen
+    .getByAltText('Pikachu crying because the page requested was not found');
+
+  expect(noFoundIMG).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
