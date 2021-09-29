baeb7ab6c0ae631552b644b9a8c961099622f790
João Ferreira <joaovtf39@gmail.com>
2021-09-29 17:49:56 -0300

    requisito 3 OK

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helper/renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+test('Requisito 3', () => {
+  renderWithRouter(<FavoritePokemons />);
+
+  const textNotFound = screen.getByText('No favorite pokemon found');
+  expect(textNotFound).toBeInTheDocument();
+});
